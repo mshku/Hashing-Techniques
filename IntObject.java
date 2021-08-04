e1cf394e9e7be6f336b5e22faf1a22952a96fd0d
mshku <88448101+mshku@users.noreply.github.com>
2021-08-04 17:41:55 -0400

    Add files via upload

---
+++ IntObject.java
@@ -0,0 +1,16 @@
+public class IntObject {
+   int data;
+
+   public IntObject (int aData) {
+      data = aData;
+   }
+
+   public int getData() {
+      return data;
+   }
+
+   public void setData(int someData) {
+      data = someData;
+   }
+}
+
